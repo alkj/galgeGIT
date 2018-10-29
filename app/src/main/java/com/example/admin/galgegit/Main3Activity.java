3d500110934aa8ef332cbb1fb505422cc107f792
Admin <s165477@student.dtu.dk>
2018-10-29 11:30:38 +0100

    fragment halvt færdigt

--- app/src/main/java/com/example/admin/galgegit/Main3Activity.java
+++ app/src/main/java/com/example/admin/galgegit/Main3Activity.java
@@ -2,12 +2,21 @@ package com.example.admin.galgegit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class Main3Activity extends AppCompatActivity {
+public class Main3Activity extends AppCompatActivity implements View.OnClickListener
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
     }
+
+        @Override
+        public void onClick(View v) {
+
+
+        }
+
 }
+
