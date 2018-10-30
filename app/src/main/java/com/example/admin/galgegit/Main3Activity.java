4e0172a7499be141ce9e8a007b28fbee897b9ebc
Admin <s165477@student.dtu.dk>
2018-10-30 13:24:01 +0100

    fragment implementation

--- app/src/main/java/com/example/admin/galgegit/Main3Activity.java
+++ app/src/main/java/com/example/admin/galgegit/Main3Activity.java
@@ -4,19 +4,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class Main3Activity extends AppCompatActivity implements View.OnClickListener
+import com.example.admin.galgegit.dummy.DummyContent;
+
+public class Main3Activity extends AppCompatActivity implements View.OnClickListener, BlankFragment.OnFragmentInteractionListener, ItemFragment.OnListFragmentInteractionListener
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
     }
+
+
+        @java.lang.Override
+        public void onFragmentInteraction(Uri uri) {
+
+        }
+
+        @java.lang.Override
+        public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
+        }
 
         @Override
         public void onClick(View v) {
 
 
         }
+
 
 }
 
